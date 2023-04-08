b4f334f289a830dabaeb1f0eb2a9198a8e921ec0
simonNolf <simon.nolf@gmail.com>
2023-04-08 10:14:45 +0200

    prise en main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <div>
+        <h2>
+            "coucou"
+        </h2>
+    </div>
 </template>
